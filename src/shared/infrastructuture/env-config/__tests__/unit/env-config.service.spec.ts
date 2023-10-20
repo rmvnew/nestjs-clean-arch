a21614da1fb65811b886e398fdb87513d514e603
Ricardo <rmvnew@gmail.com>
2023-10-20 14:54:34 -0400

    chore: EnvConfig unit tests

---
+++ src/shared/infrastructuture/env-config/__tests__/unit/env-config.service.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { EnvConfigModule } from '../../env-config.module';
+import { EnvConfigService } from '../../env-config.service';
+
+describe('EnvConfigService unit tests', () => {
+  let sut: EnvConfigService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [EnvConfigModule.forRoot()],
+      providers: [EnvConfigService],
+    }).compile();
+
+    sut = module.get<EnvConfigService>(EnvConfigService);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+  });
+
+
+  it('should return the variable PORT', () => {
+    expect(sut.getAppPort()).toBe(3500);
+  });
+
+
+  it('should return the variable NODE_ENV', () => {
+    expect(sut.getNodeEnv()).toBe('test');
+  });
+
+});
