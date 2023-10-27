42542bebb2704a322346e7467ccfb49b4795adfa
Ricardo <rmvnew@gmail.com>
2023-10-27 18:12:20 -0400

    chore: UserEntity - getters methods Unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
 
+
 export type UserProps = {
   name: string
   email: string
@@ -6,11 +7,29 @@ export type UserProps = {
   createdAt?: Date
 }
 
+
 export class UserEntity {
 
 
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  get createAt() {
+    return this.props.createdAt
   }
 
 }
