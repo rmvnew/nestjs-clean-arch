273aa02b2f32d41e0ef350806e2237a008fb7dc4
Ricardo <rmvnew@gmail.com>
2023-10-27 22:09:14 -0400

    chore: adding interface ValidatorFieldsInterface

---
+++ src/shared/domain/validators/validator-field.interface.ts
@@ -0,0 +1,13 @@
+
+
+export type FieldsErrors = {
+  [field: string]: string[]
+}
+
+export interface ValidatorFieldsInterface<PropsValidated> {
+
+  erros: FieldsErrors
+  validatedData: PropsValidated
+  validate(data: any): boolean
+
+}
